fba9a13eceecd8582999869dfd3adfc37c49bbd2
MohammadALSalamat <alomda.alslmat@gmail.com>
2021-03-07 09:48:07 +0400

    --- Auto-Git Commit ---

---
+++ app/Models/ProjectImages.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectImages extends Model
+{
+    use HasFactory;
+    public function projectSections()
+    {
+        # code...
+        return $this->belongsTo('App\Models\ProjectSection');
+    }
+
+}
