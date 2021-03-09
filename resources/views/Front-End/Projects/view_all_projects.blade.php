ea3337ad5d25f1ebfaa1a536bbe838dcc3c7ede7
MohammadALSalamat <alomda.alslmat@gmail.com>
2021-03-09 10:48:01 +0400

    --- Auto-Git Commit ---

--- resources/views/Front-End/Projects/view_all_projects.blade.php
+++ resources/views/Front-End/Projects/view_all_projects.blade.php
@@ -1,242 +1,355 @@
+@extends('layouts.front-layout.main_desgin_project')
+@section('content')
 <style>
     *,
-*:before,
-*:after {
-  box-sizing: border-box;
-}
-.product-card-container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  perspective: 850px;
-}
-.product-card-container .product-card {
-  width: 25em;
-  height: 25em;
-  background: #ccc;
-  position: relative;
-  padding: 2em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  transform-style: preserve-3d;
-}
-.product-card-container .product-card:after {
-  content: "";
-  width: 100%;
-  height: 10px;
-  border-radius: 50%;
-  position: absolute;
-  left: 0;
-  bottom: -60px;
-  box-shadow: 0 20px 20px rgba(0, 0, 0, 0.3);
-}
-.product-card-container .product-card .info-icon-container {
-  cursor: pointer;
-  position: absolute;
-  top: 0;
-  left: 0;
-  margin: 1em 1em;
-  z-index: 3;
-  height: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transform: translateZ(20px);
-}
-.product-card-container .product-card .info-icon-container:hover .info-icon .bar-1 {
-  width: 45px;
-}
-.product-card-container .product-card .info-icon-container:hover .info-icon .bar-2 {
-  width: 36px;
-}
-.product-card-container .product-card .info-icon-container:hover .info-icon .bar-3 {
-  width: 30px;
-}
-.product-card-container .product-card .info-icon-container .info-icon {
-  cursor: pointer;
-  position: relative;
-  background: red;
-}
-.product-card-container .product-card .info-icon-container .info-icon .bar-1,
-.product-card-container .product-card .info-icon-container .info-icon .bar-2,
-.product-card-container .product-card .info-icon-container .info-icon .bar-3 {
-  background: white;
-  height: 1px;
-  transition: all 0.4s ease;
-}
-.product-card-container .product-card .info-icon-container .info-icon .bar-2 {
-  width: 30px;
-}
-.product-card-container .product-card .info-icon-container .info-icon .bar-1,
-.product-card-container .product-card .info-icon-container .info-icon .bar-3 {
-  width: 20px;
-  position: absolute;
-}
-.product-card-container .product-card .info-icon-container .info-icon .bar-1 {
-  top: 10px;
-}
-.product-card-container .product-card .info-icon-container .info-icon .bar-3 {
-  bottom: 10px;
-}
-.product-card-container .product-card .info-icon-container p {
-  font-size: 0.8rem;
-  font-weight: 300;
-  letter-spacing: 0.2rem;
-  margin-left: 30px;
-}
-.product-card-container .product-card::before {
-  content: "";
-  background: rgba(0, 0, 0, 0.8);
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 2;
-}
-.product-card-container .product-card .header {
-  display: flex;
-  flex-direction: column;
-  z-index: 2;
-  position: relative;
-  color: white;
-}
-.product-card-container .product-card .header h1 {
-  font-size: 2em;
-  text-transform: uppercase;
-  font-weight: 700;
-  margin-bottom: 1rem;
-  transform: translateZ(50px);
-}
-.product-card-container .product-card .header button {
-  color: white;
-  border-radius: 50px;
-  border: 1px solid white;
-  background: rgba(0, 0, 0, 0);
-  width: 15em;
-  height: 3em;
-  margin: 0 auto;
-  text-transform: uppercase;
-  letter-spacing: 0.15rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  margin-bottom: 1rem;
-  transition: all 0.3s ease-in-out;
-  transform: translateZ(10px);
-}
-.product-card-container .product-card .header button:hover {
-  transform: translateZ(100px);
-  background: white;
-  color: black;
-}
-.product-card-container .product-card .header button:focus {
-  outline: none;
-}
-.product-card-container .product-card .header button .fa-shopping-bag {
-  margin-left: 1rem;
-  transform: translateZ(50px);
-}
-.product-card-container .product-card .header em {
-  font-style: italic;
-  text-align: center;
-  font-weight: 300;
-  transform: translateZ(20px);
-}
-.product-card-container .product-card .product-img {
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  bottom: -5rem;
-  z-index: 3;
-  transition: all 0.3s ease-in-out;
-  perspective: 850px;
-  transform: translateZ(40px);
-}
-.product-card-container .product-card .product-img img {
-  width: 15em;
-  filter: drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.5));
-}
-.product-card-container .product-card .product-img:after {
-  content: "";
-  width: 100%;
-  height: 10px;
-  border-radius: 50%;
-  position: absolute;
-  left: 0;
-  bottom: -10px;
-}
-.product-card-container .product-card .bg-img {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  position: absolute;
-}
-.product-card-container .product-card .bg-img img {
-  width: 25em;
-  z-index: 1;
-}
+    *:before,
+    *:after {
+        box-sizing: border-box;
+    }
 
-.coords, .coords-2 {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  margin: 1rem;
-}
+    .product-card-container {
+        width: 100%;
+    }
 
-.coords-2 {
-  margin-bottom: 2rem;
-}
+    .product-card-container .product-card {
+        height: 25em;
+        background: #ccc;
+        position: relative;
+        padding: 2em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        transform-style: preserve-3d;
+    }
+
+    .product-card-container .product-card:after {
+        content: "";
+        width: 100%;
+        height: 10px;
+        border-radius: 50%;
+        position: absolute;
+        left: 0;
+        bottom: -60px;
+        box-shadow: 0 20px 20px rgba(0, 0, 0, 0.3);
+    }
+
+    .product-card-container .product-card .info-icon-container {
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin: 1em 1em;
+        z-index: 3;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transform: translateZ(20px);
+    }
+
+    .product-card-container .product-card .info-icon-container:hover .info-icon .bar-1 {
+        width: 45px;
+    }
+
+    .product-card-container .product-card .info-icon-container:hover .info-icon .bar-2 {
+        width: 36px;
+    }
+
+    .product-card-container .product-card .info-icon-container:hover .info-icon .bar-3 {
+        width: 30px;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon {
+        cursor: pointer;
+        position: relative;
+        background: red;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon .bar-1,
+    .product-card-container .product-card .info-icon-container .info-icon .bar-2,
+    .product-card-container .product-card .info-icon-container .info-icon .bar-3 {
+        background: white;
+        height: 1px;
+        transition: all 0.4s ease;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon .bar-2 {
+        width: 30px;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon .bar-1,
+    .product-card-container .product-card .info-icon-container .info-icon .bar-3 {
+        width: 20px;
+        position: absolute;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon .bar-1 {
+        top: 10px;
+    }
+
+    .product-card-container .product-card .info-icon-container .info-icon .bar-3 {
+        bottom: 10px;
+    }
+
+    .product-card-container .product-card .info-icon-container p {
+        font-size: 0.8rem;
+        font-weight: 300;
+        letter-spacing: 0.2rem;
+        margin-left: 30px;
+    }
+
+    .product-card-container .product-card::before {
+        content: "";
+        background: rgba(0, 0, 0, 0.8);
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 2;
+    }
+
+    .product-card-container .product-card .header {
+        display: flex;
+        flex-direction: column;
+        z-index: 2;
+        position: relative;
+        color: white;
+    }
+
+    .product-card-container .product-card .header h1 {
+        font-size: 2em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin-bottom: 1rem;
+        transform: translateZ(50px);
+    }
+
+    .product-card-container .product-card .header button {
+        color: white;
+        border-radius: 50px;
+        border: 1px solid white;
+        background: rgba(0, 0, 0, 0);
+        width: 15em;
+        height: 3em;
+        margin: 0 auto;
+        text-transform: uppercase;
+        letter-spacing: 0.15rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        margin-bottom: 1rem;
+        transition: all 0.3s ease-in-out;
+        transform: translateZ(10px);
+    }
+
+    .product-card-container .product-card .header button:hover {
+        transform: translateZ(100px);
+        background: white;
+        color: black;
+    }
+
+    .product-card-container .product-card .header button:focus {
+        outline: none;
+    }
+
+    .product-card-container .product-card .header button .fa-shopping-bag {
+        margin-left: 1rem;
+        transform: translateZ(50px);
+    }
+
+    .product-card-container .product-card .header em {
+        font-style: italic;
+        text-align: center;
+        font-weight: 300;
+        transform: translateZ(20px);
+    }
+
+    .product-card-container .product-card .product-img {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: -5rem;
+        z-index: 3;
+        transition: all 0.3s ease-in-out;
+        perspective: 850px;
+        transform: translateZ(40px);
+    }
+
+    .product-card-container .product-card .product-img img {
+        width: 15em;
+        filter: drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.5));
+    }
+
+    .product-card-container .product-card .product-img:after {
+        content: "";
+        width: 100%;
+        height: 10px;
+        border-radius: 50%;
+        position: absolute;
+        left: 0;
+        bottom: -10px;
+    }
+
+    .product-card-container .product-card .bg-img {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        position: absolute;
+    }
+
+    .product-card-container .product-card .bg-img img {
+        width: 25em;
+        z-index: 1;
+    }
+
+    .coords,
+    .coords-2 {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        margin: 1rem;
+    }
+
+    .coords-2 {
+        margin-bottom: 2rem;
+    }
 </style>
+<div class="container">
+    <div class="row">
+        <div class="col-3 gap-4">
+            <div class="product-card-container">
+                <div class="product-card">
+                    <div class="info-icon-container">
+                        <div class="info-icon">
+                            <div class="bar-1"></div>
+                            <div class="bar-2"></div>
+                            <div class="bar-3"></div>
+                        </div>
+                        <!-- 			<p>Customize</p> -->
+                    </div>
+                    <div class="header">
+                        <h1>Headphones</h1>
+                        <button>Add To Bag <i class="fa fa-shopping-bag"></i></button>
+                        <em>$70.00</em>
+                    </div> <!-- end of header -->
+                    <div class="product-img">
+                        <img src="logo/9973523.png" alt="" />
+                    </div>
+                    <div class="bg-img">
+                        <img src="https://goo.gl/U7XMDL" alt="" />
+                    </div>
 
+                </div> <!-- end of product-card -->
+            </div><!-- end of product-card-container -->
+        </div>
+        <div class="col-3">
+            <div class="product-card-container">
+                <div class="product-card">
+                    <div class="info-icon-container">
+                        <div class="info-icon">
+                            <div class="bar-1"></div>
+                            <div class="bar-2"></div>
+                            <div class="bar-3"></div>
+                        </div>
+                        <!-- 			<p>Customize</p> -->
+                    </div>
 
-<div class="product-card-container">
-	<div class="product-card">
-		<div class="info-icon-container">
-			<div class="info-icon">
-				<div class="bar-1"></div>
-				<div class="bar-2"></div>
-				<div class="bar-3"></div>
-			</div>
-<!-- 			<p>Customize</p> -->
-		</div>
+                    <div class="header">
+                        <h1>Headphones</h1>
+                        <button>Add To Bag <i class="fa fa-shopping-bag"></i></button>
+                        <em>$70.00</em>
+                    </div> <!-- end of header -->
+                    <div class="product-img">
+                        <img src="logo/9973523.png" alt="" />
+                    </div>
+                    <div class="bg-img">
+                        <img src="https://goo.gl/U7XMDL" alt="" />
+                    </div>
 
-		<div class="header">
-			<h1>Headphones</h1>
-			<button>Add To Bag <i class="fa fa-shopping-bag"></i></button>
-			<em>$70.00</em>
-		</div> <!-- end of header -->
-		<div class="product-img">
-			<img src="https://goo.gl/HfpFyP" alt="" />
-		</div>
-		<div class="bg-img">
-			<img src="https://goo.gl/U7XMDL" alt="" />
-		</div>
+                </div> <!-- end of product-card -->
+            </div><!-- end of product-card-container -->
+        </div>
+        <div class="col-3">
+            <div class="product-card-container">
+                <div class="product-card">
+                    <div class="info-icon-container">
+                        <div class="info-icon">
+                            <div class="bar-1"></div>
+                            <div class="bar-2"></div>
+                            <div class="bar-3"></div>
+                        </div>
+                        <!-- 			<p>Customize</p> -->
+                    </div>
 
-	</div> <!-- end of product-card -->
-</div><!-- end of product-card-container -->
+                    <div class="header">
+                        <h1>Headphones</h1>
+                        <button>Add To Bag <i class="fa fa-shopping-bag"></i></button>
+                        <em>$70.00</em>
+                    </div> <!-- end of header -->
+                    <div class="product-img">
+                        <img src="logo/9973523.png" alt="" />
+                    </div>
+                    <div class="bg-img">
+                        <img src="https://goo.gl/U7XMDL" alt="" />
+                    </div>
 
+                </div> <!-- end of product-card -->
+            </div><!-- end of product-card-container -->
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="product-card-container">
+            <div class="product-card">
+                <div class="info-icon-container">
+                    <div class="info-icon">
+                        <div class="bar-1"></div>
+                        <div class="bar-2"></div>
+                        <div class="bar-3"></div>
+                    </div>
+                    <!-- 			<p>Customize</p> -->
+                </div>
+
+                <div class="header">
+                    <h1>Headphones</h1>
+                    <button>Add To Bag <i class="fa fa-shopping-bag"></i></button>
+                    <em>$70.00</em>
+                </div> <!-- end of header -->
+                <div class="product-img">
+                    <img src="logo/9973523.png" alt="" />
+                </div>
+                <div class="bg-img">
+                    <img src="https://goo.gl/U7XMDL" alt="" />
+                </div>
+
+            </div> <!-- end of product-card -->
+        </div><!-- end of product-card-container -->
+    </div>
+</div>
+
+    </div>
+
+</div>
 <!-- <p class="coords"></p>
 <p class="coords-2"></p> -->
-<script>
-    const card = $('.product-card'),
-			coord = $('.coords'),
-			coord2 = $('.coords-2')
+<script type="text/babel">
+const card = $('.product-card'),
+coord = $('.coords'),
+coord2 = $('.coords-2');
 
-$(document).on('mousemove', function(e) {
-	let ax = -($(window).innerWidth()/2 - e.pageX)/20,
-			ay = ($(window).innerHeight()/2 - e.pageY)/20
-	card.css({
-		transform: `rotateY(${ax}deg) rotateX(${ay}deg)`
-	});
-	// coord.text(`ax: ${ax}, ay: ${ay}`)
-	coord.text(`half x: ${-($(window).innerWidth()/2 - e.pageX)}`)
-	coord2.text(`pageX: ${e.pageX}, pageY: ${e.pageY}`)
-})
+$(document).on('mousemove', function (e) {
+  let ax = -($(window).innerWidth() / 2 - e.pageX) / 20,
+  ay = ($(window).innerHeight() / 2 - e.pageY) / 20;
+  card.css({
+    transform: `rotateY(${ax}deg) rotateX(${ay}deg)` });
+
+  // coord.text(`ax: ${ax}, ay: ${ay}`)
+  coord.text(`half x: ${-($(window).innerWidth() / 2 - e.pageX)}`);
+  coord2.text(`pageX: ${e.pageX}, pageY: ${e.pageY}`);
+});
 </script>
+@endsection
+
---
+++ resources/views/layouts/front-layout/main_desgin_project.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>9Yards Events</title>
+
+    <!-- Font awesome -->
+    <link href="{{ url('css/font-awesome.css') }}" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link id="switcher" href="{{ url('css/theme-color/default-theme.css') }}" rel="stylesheet">
+
+    <!-- Main style sheet -->
+    <link href="{{ url('css/style.css') }}" rel="stylesheet">
+    <link href="{{ url('css/tailwind.css') }}" rel="stylesheet">
+    <!-- Google Font -->
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet"
+href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+
+<link rel="stylesheet" href="{{ url('admin-style/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet"
+href="{{ url('admin-style/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<!-- JQVMap -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/jqvmap/jqvmap.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ url('admin-style/dist/css/adminlte.min.css') }}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/daterangepicker/daterangepicker.css') }}">
+<!-- summernote -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/summernote/summernote-bs4.min.css') }}">
+<!-- dropzonejs -->
+<link rel="stylesheet" href="{{ url('admin-style/plugins/dropzone/min/dropzone.min.css') }}">
+<!-- Toastr -->
+<link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js'></script>
+
+
+<!-- here to add extra links from other pages -->
+<!-- here to add Style for editor -->
+<!-- Summernote -->
+<script src="{{ url('admin-style/plugins/summernote/summernote-bs4.min.js') }}"></script>
+<!-- buttons style-->
+<link rel="stylesheet" href="{{ url('css/backend_css/theme.css') }}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Imbue:wght@500&family=Pinyon+Script&family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="">
+        {{-- <!-- wpf loader Two -->
+        <div id="wpf-loader-two">
+            <div class="wpf-loader-two-inner">
+                <span>Loading</span>
+            </div>
+        </div> --}}
+
+    @include('layouts.front-layout.top-navbar') <!-- menu code  -->
+        @yield('content')
+
+
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+    </div> <!-- end of div Wrapper -->
+
+
+
+    @include('layouts.front-layout.footer')<!-- Footer code  -->
+
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ url('js/bootstrap.js') }}"></script>
+    <!-- animate jQuery plugin -->
+    <script src="{{ url('js/lib/anime.es.js') }}"></script>
+    <script src="{{ url('js/lib/anime.js') }}"></script>
+    <script src="{{ url('js/lib/anime.min.js') }}"></script>
+    <!-- SmartMenus jQuery Bootstrap Addon -->
+    <!-- To Slider JS -->
+    <script src="{{ url('js/sequence.js') }}"></script>
+    <script src="{{ url('js/sequence-theme.modern-slide-in.js') }}"></script>
+    <!-- Product view slider -->
+    <script type="text/javascript" src="{{ url('js/jquery.simpleGallery.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/jquery.simpleLens.js') }}"></script>
+    <!-- slick slider -->
+    <script type="text/javascript" src="{{ url('js/slick.js') }}"></script>
+    <!-- Price picker slider -->
+    <!-- Custom js -->
+    <script src="{{ url('js/custom.js') }}"></script>
+
+    <script src="{{ url('js/main.js') }}"></script>
+    <!-- Important link to work Toastr-->
+    <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+
+    {!! Toastr::message() !!}
+    @yield('script')
+
+</body>
+
+</html>
