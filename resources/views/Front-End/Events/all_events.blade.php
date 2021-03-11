6ea6f886226c6ab01b9b7d520c21655962c55fd9
MohammadALSalamat <alomda.alslmat@gmail.com>
2021-03-11 13:17:18 +0400

    --- Auto-Git Commit ---

--- resources/views/Front-End/Events/all_events.blade.php
+++ resources/views/Front-End/Events/all_events.blade.php
@@ -286,39 +286,44 @@ img:hover{
     </div>
   </div>
 
-  {{-- <section id="portfolio" class="portfolio">
-  @foreach ($Projects as $sections)
-  @if($sections->status==1)
-  @foreach ($sections->projectImages as $image)
-  <div class="col-3 portfolio-item filter-app">
-      @if($image->status == 1)
-      <div class="portfolio-img"><img src="{{ asset('img/projects/'.$image->Image) }}" class="img-fluid" alt="{{ $image->Image }}" ></div>
-      <div class="portfolio-info">
-        <h4>App 1</h4>
-        <p>App</p>
-        <a href="{{ asset('img/projects/'.$image->Image) }}" data-gall="portfolioGallery" class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+ <!--
+  Please note: this code is in no way ready to be used as is in production on your website. It will need to be adapted to be cross browser compliant & accessible. I just wanted to share how one might go about this effect with CSS & JS and no other dependencies
+-->
+{{-- <div class="container">
+    <div class="card-column column-0">
+      <div class="card card-color-0">
+        <div class="border"></div>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53148/deathtostock-00.jpg" />
+        <h1>Hey now, you're an allstar</h1>
       </div>
-      @else
-      <img src="" alt="" style="display: none">
-      @endif
+      <div class="card card-color-2">
+        <div class="border"></div>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53148/deathtostock-02.jpg" />
+        <h1>Hey now, you're a rock star</h1>
+      </div>
     </div>
-      @endforeach
-  @else
-  <div style="display: none">
-  </div>
-  @endif
-  @endforeach
-  <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-    <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt=""></div>
-    <div class="portfolio-info">
-      <h4>App 1</h4>
-      <p>App</p>
-      <a href="assets/img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-      <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+    <div class="card-column column-1">
+      <div class="card card-color-1">
+        <div class="border"></div>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53148/deathtostock-01.jpg" />
+        <h1>Get your game on, go play</h1>
+      </div>
+      <div class="card card-color-3">
+        <div class="border"></div>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53148/deathtostock-03.jpg" />
+        <h1>Get the show on, get paid</h1>
+      </div>
     </div>
   </div>
-  </section> --}}
+
+  <div id="cover" class="cover"></div>
+
+  <div id="open-content" class="open-content">
+    <a href="#" id="close-content" class="close-content"><span class="x-1"></span><span class="x-2"></span></a>
+    <img id="open-content-image" src="" />
+    <div class="text" id="open-content-text">
+    </div>
+  </div> --}}
   <!-- Second option -->
 
 <div class="bg-images">
